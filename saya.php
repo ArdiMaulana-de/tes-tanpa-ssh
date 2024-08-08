6fde483ad730f5ac4dff88ff10e53d33f65a7a3f
ardi <ardimaulana26@gmail.com>
2024-08-08 14:15:15 +0700

    first commit

---
+++ saya.php
@@ -0,0 +1,17 @@
+<?php
+// string
+$saya = 'aku cinta kamu';
+$kamu = 102 ;
+$kita = 10.3;
+
+$ee = ["saya suka", 10 ,10.3];
+
+class aka {
+    public $ia = 'aku suka kamu';
+    public $ai = 10 + 1 ;
+    public $aii = 9.5 ;
+    public $aiii = false ;
+}
+
+$aka = new aka ();
+
